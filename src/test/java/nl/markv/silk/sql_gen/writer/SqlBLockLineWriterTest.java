d0f7e2a8b40561d7122da29be4ef8aa4bc6e771a
Mark <mverleg.noreply@gmail.com>
2020-05-09 22:12:04 +0200

    Lines and blocks working!

--- src/test/java/nl/markv/silk/sql_gen/writer/SqlBLockLineWriterTest.java
+++ src/test/java/nl/markv/silk/sql_gen/writer/SqlBLockLineWriterTest.java
@@ -49,23 +49,42 @@ class SqlBLockLineWriterTest {
 	@Test
 	void lineBlockSingleLines() {
 		SqlStringWriter sql = new SqlStringWriter();
-		sql.addLine("(");
+		sql.add("(\n\t");
 		sql.lineBlock(() ->
 			sql.line(() -> sql.add("int id auto_increment"))
 		);
 		sql.addLine(")");
-		assertEquals("(\nint id auto_increment\n)\n", sql.build());
+		assertEquals("(\n\tint id auto_increment\n)\n", sql.build());
 	}
 
 	@Test
 	void lineBlockTwoLines() {
 		SqlStringWriter sql = new SqlStringWriter();
-		sql.addLine("(");
+		sql.add("(\n\t");
 		sql.lineBlock(() -> {
 			sql.line(() -> sql.add("int id auto_increment"));
 			sql.line(() -> sql.add("text name not null"));
 		});
 		sql.addLine(")");
-		assertEquals("(\nint id auto_increment,\n\ttext name not null\n)\n", sql.build());
+		assertEquals("(\n\tint id auto_increment,\n\ttext name not null\n)\n", sql.build());
+	}
+
+	@Test
+	void lineTwoBlockTwoLines() {
+		SqlStringWriter sql = new SqlStringWriter();
+		sql.add("(\n\t");
+		sql.lineBlock(() -> {
+			sql.line(() -> sql.add("int id auto_increment"));
+			sql.line(() -> sql.add("text name not null"));
+		});
+		sql.addLine(")");
+		sql.statements(() -> sql.add("drop table Users"));
+		sql.add("(\n\t");
+		sql.lineBlock(() -> {
+			sql.line(() -> sql.add("int age"));
+			sql.line(() -> sql.add("int length_cm"));
+		});
+		sql.addLine(")");
+		assertEquals("(\n\tint id auto_increment,\n\ttext name not null\n)\ndrop table Users;\n(\n\tint age,\n\tint length_cm\n)\n", sql.build());
 	}
 }
